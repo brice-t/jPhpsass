9954f392d7955d7bf5924cf5423968bf12d058fb
Brice TENCE <brice@tence>
2012-09-17 22:08:23 +0200

    + first commit. Basic behaviour seems to work already ...

---
+++ README.md
@@ -0,0 +1,64 @@
+What is phpsass4jelix ?
+==============================
+
+This project is a plugin for [Jelix](http://jelix.org) PHP framework. It allows you to use easily [Sass](http://sass-lang.com/) dynamic stylesheet language in Jelix (using [phpsass](http://phpsass.com/) compiler).
+
+This is an htmlresponse plugin.
+
+
+
+Installation
+============
+
+Under Jelix default configuration, create an "htmlresponse" directory in your project's "plugins" directory.
+Clone this repository in that directory.
+
+Note that you should have your app plugin directory in your modulesPath (defaultconfig.ini.php or entry point's config.ini.php) to get it working.
+The value should be at least :
+
+    modulesPath="app:modules/"
+
+
+
+Usage
+=====
+
+When including a CSS file (e.g. with addCSSLink()) you should set 'sass'=>true as a param.
+
+Another way of having a file compiled with Sass is including as file with .scss or .sass extension. You can set expected extensions in the comma-separated value of _phpsass\_extensions_ under the _jResponseHtml_ section.
+
+E.g. in your response :
+
+`$this->addCSSLink($gJConfig->urlengine['basePath'].'themes/'.$gJConfig->theme.'/Css/style.sass');`
+
+or
+
+`$this->addCSSLink($gJConfig->urlengine['basePath'].'themes/'.$gJConfig->theme.'/Css/style.sassFile', array( 'sass' => true ));`
+
+
+Your config file must activate phpsass plugin :
+
+    [jResponseHtml]
+    plugins=phpsass
+
+N.B. : the directories containing Sass files should be writable by your web server ! Indeed, compiled files will be written in that very same directory so that relative urls go on working ...
+
+
+
+
+Config
+======
+
+You can configure phpsass's behviour regarding compilation:
+
+    [jResponseHtml]
+    ;...
+    ; always|onchange|once
+    phpsass_compile=always
+
+If phpsass\_compile's value is not valid or empty, its default value is onchange.
+
+* always : compile Sass file on all requests
+* onchange : compile Sass file only if it has changed
+* once : compile Sass file once and never compile it again (until compiled file is removed)
+
---
+++ phpsass/phpsass.htmlresponse.php
@@ -0,0 +1,331 @@
+<?php
+/**
+* @package     
+* @subpackage  
+* @author      Brice Tencé
+* @copyright   2012 Brice Tencé
+* @link        
+* @licence     GNU Lesser General Public Licence see LICENCE file or http://www.gnu.org/licenses/lgpl.html
+*/
+
+/**
+* plugin for jResponseHTML, which processes Sass files
+*/
+
+require_once 'phpsass/SassParser.php';
+
+define('PHPSASS_COMPILE_ALWAYS', 1 );
+define('PHPSASS_COMPILE_ONCHANGE', 2 ); //default value : phpsass4jelix default behaviour
+define('PHPSASS_COMPILE_ONCE', 3 );
+
+class phpsassHTMLResponsePlugin implements jIHTMLResponsePlugin {
+
+    protected $response = null;
+
+    public function __construct(jResponse $c) {
+        $this->response = $c;
+    }
+
+    /**
+     * called just before the jResponseBasicHtml::doAfterActions() call
+     */
+    public function afterAction() {
+    }
+
+    /**
+     * called when the content is generated, and potentially sent, except
+     * the body end tag and the html end tags. This method can output
+     * directly some contents.
+     */
+    public function beforeOutput() {
+        if (!($this->response instanceof jResponseHtml))
+            return;
+        global $gJConfig;
+
+        $compileFlag = PHPSASS_COMPILE_ONCHANGE;
+        if( isset($gJConfig->jResponseHtml['phpsass_compile']) ) {
+            switch($gJConfig->jResponseHtml['phpsass_compile']) {
+            case 'always':
+                $compileFlag = PHPSASS_COMPILE_ALWAYS;
+                break;
+            case 'onchange':
+                $compileFlag = PHPSASS_COMPILE_ONCHANGE;
+                break;
+            case 'once':
+                $compileFlag = PHPSASS_COMPILE_ONCE;
+                break;
+            }
+        }
+
+        $sassExtensions = array('sass', 'scss');
+        if( isset($gJConfig->jResponseHtml['phpsass_extensions']) ) {
+            $extString = $gJConfig->jResponseHtml['phpsass_extensions'];
+            $sassExtensions = array_map( 'trim', explode(',', $extString) );
+        }
+
+        $optionsArray = array( 
+                            'style' => 'nested', // nested (default), compact, compressed, or expanded
+                            'debug' => false,
+                            'watchdog' => false
+                        );
+        if( isset($gJConfig->jResponseHtml['phpsass_style']) ) {
+            $optionsArray['style'] = $gJConfig->jResponseHtml['phpsass_style'];
+        }
+        if( isset($gJConfig->jResponseHtml['phpsass_debug']) ) {
+            $optionsArray['debug'] = $gJConfig->jResponseHtml['phpsass_debug'];
+        }
+        if( isset($gJConfig->jResponseHtml['phpsass_watchdog']) ) {
+            $optionsArray['watchdog'] = $gJConfig->jResponseHtml['phpsass_watchdog'];
+        }
+
+        $inputCSSLinks = $this->response->getCSSLinks();
+        $outputCSSLinks = array();
+
+        foreach( $inputCSSLinks as $inputCSSLinkUrl=>$CSSLinkParams ) {
+            $CSSLinkUrl = $inputCSSLinkUrl;
+            if( in_array( pathinfo($inputCSSLinkUrl, PATHINFO_EXTENSION), $sassExtensions ) ||
+                (isset($CSSLinkParams['sass']) && $CSSLinkParams['sass']) ) {
+                    //we suppose url starts with basepath ...
+                    if( substr($CSSLinkUrl, 0, strlen($gJConfig->urlengine['basePath'])) != $gJConfig->urlengine['basePath'] ) {
+                        throw new Exception("File $CSSLinkUrl seems not to be located in your basePath : it can not be processed with phpsass");
+                    } else {
+                        $filePath = jApp::wwwPath() . substr($CSSLinkUrl, strlen($gJConfig->urlengine['basePath']));
+
+                        $outputSuffix = '.css';
+                        $outputPath = $filePath . $outputSuffix;
+
+                        try {
+                            $compile = true;
+                            if( is_file($outputPath) ) {
+                                if( ($compileFlag == PHPSASS_COMPILE_ALWAYS) ) {
+                                    unlink($outputPath);
+                                } elseif( ($compileFlag == PHPSASS_COMPILE_ONCE) ) {
+                                    $compile = false;
+                                }
+                                //PHPSASS_COMPILE_ONCHANGE is phpsass4jelix's natural behaviour. So we let him do ...
+                            }
+                            if( $compile ) {
+                                $this->compileSass($filePath, $outputPath, $optionsArray);
+                            }
+                            $CSSLinkUrl = $CSSLinkUrl . $outputSuffix;
+                        } catch (exception $ex) {
+                            trigger_error("sass fatal error on file $filePath:<br />".$ex->getMessage(), E_USER_ERROR);
+                        }
+                    }
+                    unset($CSSLinkParams['sass']);
+            }
+
+            $outputCSSLinks[$CSSLinkUrl] = $CSSLinkParams;
+        }
+
+        $this->response->setCSSLinks( $outputCSSLinks );
+    }
+
+
+
+    private function compileSass( $filePath, $outputPath, $optionsArray ) {
+        extract ($optionsArray);
+
+        try {
+            $options = array(
+                'style' => $style,
+                'cache' => FALSE,
+                'syntax' => pathinfo($filePath, PATHINFO_EXTENSION),
+                'debug' => FALSE,
+                'debug_info' => $debug,
+                'load_paths' => array(dirname($filePath)),
+                'filename' => array('dirname'=>dirname($filePath), 'basename'=>basename($filePath)),
+                'load_path_functions' => array(),//'sassy_load_callback'),
+                'functions' => sassy_get_functions(),
+                'callbacks' => array(
+                    'warn' => $watchdog ? 'watchdog_warn' : NULL,
+                    'debug' => $watchdog ? 'watchdog_debug' : NULL,
+                )
+            );
+            // Execute the compiler.
+            $parser = new SassParser($options);
+            file_put_contents( $outputPath, $parser->toCss(file_get_contents($filePath), false) );
+            return;
+        }
+        catch (Exception $e) {
+            trigger_error( "Sass error for '$filePath' : " . $e->getMessage(), E_USER_ERROR );
+        }
+
+    }
+
+
+
+
+    /**
+     * called just before the output of an error page
+     */
+    public function atBottom() {
+    }
+
+    /**
+     * called just before the output of an error page
+     */
+    public function beforeOutputError() {
+    }
+}
+
+
+
+/**
+ * Callback for @warn directive.
+ */
+function watchdog_warn($message, $context) {
+  _watchdog($message, $context, 'warn', E_USER_WARNING);
+}
+
+/**
+ * Callback for @debug directive.
+ */
+function watchdog_debug($message, $context) {
+  _watchdog($message, $context, 'debug', E_USER_NOTICE);
+}
+
+/**
+ * Handler for @warn/@debug directive callbacks.
+ */
+function _watchdog($message, $context, $level) {
+    $line = $context->node->token->line;
+    $filename = $context->node->token->filename;
+    $message = "Line $line of $filename : %message";
+    trigger_error( $message, $level );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Returns all functions to be used inside the parser.
+ * @author Joon Park (dvessel), richthegeek, fubhy
+ */
+function sassy_get_functions() {
+    return array();
+  $functions =& drupal_static(__FUNCTION__);
+
+  if (!isset($function)) {
+    foreach (module_invoke_all('sassy_functions') as $info) {
+      $info = (object) $info;
+      $functions[$info->name] = $info->callback;
+    }
+    foreach (array_merge($GLOBALS['base_theme_info'], array($GLOBALS['theme_info'])) as $theme) {
+      $function = $theme->name . '_sassy_functions';
+      if (function_exists($function) && $data = $function()) {
+        foreach ($data as $info) {
+          $info = (object) $info;
+          $functions[$info->name] = $info->callback;
+        }
+      }
+    }
+  }
+  return $functions;
+}
+
+/**
+ * Called from inside SassParser when a file is trying to be loaded.
+ *
+ * @param $file
+ *    The file trying to be loaded, eg 'myfile/bla.scss'
+ *
+ * @return
+ *    An array of 0 - n filenames to load.
+ *    If no valid files are found return array() or FALSE
+ */
+function sassy_load_callback($file) {
+  $file = explode('/', $file, 2);
+  $namespace = preg_replace('/[^0-9a-z]+/', '_', strtolower(array_shift($file)));
+  # check for implementing modules specific to namespace and invoke looking for a paths array.
+  foreach (module_implements('sassy_resolve_path_' . $namespace) as $module) {
+    $hook = $module . '_sassy_resolve_path_' . $namespace;
+    if (function_exists($hook) && $paths = call_user_func($hook, $file[0])) {
+      return (array) $paths;
+    }
+  }
+  # check for implenting modules for the generic hook, looking for a path array.
+  foreach (module_implements('sassy_resolve_path') as $module) {
+    $hook = $module . '_sassy_resolve_path';
+    if (function_exists($hook) && $paths = call_user_func($hook, $file)) {
+      return (array) $paths;
+    }
+  }
+  # check for modules or themes named $namespace and try directly finding a file.
+  if (!($path = drupal_get_path('module', $namespace))) {
+    $path = drupal_get_path('theme', $namespace);
+  }
+  if (!$path && $namespace == 'public') {
+    $path = 'public:/';
+  }
+  if ($path) {
+    $path = $path . '/' . $file[0];
+    if (file_exists($path)) {
+      $path = drupal_realpath($path);
+      return array($path);
+    }
+  }
+  return FALSE;
+}
+
+/**
+ * Implementation of hook_sassy_resolve_path_NAMESPACE().
+ */
+function sassy_sassy_resolve_path_sassy($file) {
+  return sassy_registered_includes(basename($file));
+}
+
+/**
+ * Fetches, caches and returns all SASS / SCSS libraries from all enabled
+ * modules and the theme trail.
+ *
+ * @return
+ *   An array of all library files, sorted by their basename.
+ */
+function sassy_registered_includes($base = NULL) {
+  $includes = &drupal_static(__FUNCTION__);
+  if (!isset($includes)) {
+    if ($cache = cache_get('sassy_libraries:' . $GLOBALS['theme_key'])) {
+      $includes = $cache->data;
+    }
+    else {
+      $includes = array();
+      // Load libraries from all enabled modules and themes.
+      foreach (array_merge(module_list(), $GLOBALS['base_theme_info'], array($GLOBALS['theme_info'])) as $info) {
+        $type = is_object($info) ? 'theme' : 'module';
+        $name = $type == 'theme' ? $info->name : $info;
+        $info = $type == 'theme' ? $info->info : system_get_info('module', $name);
+        if (!empty($info['sassy'])) {
+          foreach ($info['sassy'] as $include) {
+            $path = drupal_get_path($type, $name) . '/' . $include;
+            if (is_file($path)) {
+              $includes[basename($path)][] = $path;
+            }
+          }
+        }
+      }
+      drupal_alter('sassy_includes', $includes);
+      cache_set('sassy_includes:' . $GLOBALS['theme_key'], $includes);
+    }
+  }
+  if (isset($base) && isset($includes[$base])) {
+    return $includes[$base];
+  }
+  else if (!isset($base)) {
+    return $includes;
+  }
+}
